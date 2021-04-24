f870c453328d4a9e88ff6b5ea8dd88cd088703ed
baypp07 <pphouone@gmail.com>
2021-04-25 00:49:09 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +6,23 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+    @ViewChild('videoPlayer', { static: false }) videoplayer: any;
+  // public startedPlay:boolean = false;
+  public show:boolean = true;
+  public showVideo:boolean = false;
+  isEnded:boolean=false;
+
+    toggleVideo(videoplayer){
+    var myVideo: any = document.getElementById("prez-KG");
+    if (myVideo.paused) myVideo.play(), this.show = false, this.showVideo=true;
+    else myVideo.pause(), this.show=true, this.showVideo=false;
+  }
+  
+  videoEnd(event: any) {
+    this.isEnded = false;
+    this.show = true;
+    this.showVideo=false;
+  }
+
 }
+
